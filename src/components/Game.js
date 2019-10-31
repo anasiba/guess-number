cf4065f61606cfdf6dfac402f19d2853e34584a7
anasiba <anasibarki1997@gmail.com>
2019-10-31 22:57:11 +0100

    Initial commit

---
+++ src/components/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+function Game() {
+  return (
+    <div className="App">
+      <Header/>
+     </div>
+  );
+}
+
+export default Game;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Guess Number App </h1>
+      <p>
+        you's a little of your brain to solve this problem{" "}
+        <span role="img" aria-label="brain-emoji">
+          🧠
+        </span>
+      </p>
+    </div>
+  );
+}
